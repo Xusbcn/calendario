f528c44b4e81038359af6e5de020ce3671ce53f1
root <jesusenmanuelsoto@gmail.com>
2018-09-27 17:58:56 +0200

    tercer commit

---
+++ calendar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>my calendar</title>
+
+ <
+</head>
+<body>
+<h1>CALENDARIO</h1>
+	<?php
+
+
+		echo "<table border=1 width=294>";
+ 		echo "<tr><th colspan=7> calendario semanal </th></tr>";
+ 		echo "<tr><td width=42>DOMINGO</td><td width=42>LUNES</td><td 
+     	width=42>MARTES</td><td width=42>MIERCOLES</td><td width=42>JUEVES</td><td 
+     	width=42>VIERNES</td><td width=42>SABADO</td></tr>";
+     	
+     	
+
+ 
+ 
+ 
+ ?>
+ </tr>
+     
+</body>
+</html>
