f117deb776837de6026b0ec882f721f209d1a919
root <jesusenmanuelsoto@gmail.com>
2018-10-05 19:32:49 +0200

    pizza commit

--- processa_dades.php
+++ processa_dades.php
@@ -7,6 +7,14 @@
 	<?php
 	$var1=$_REQUEST['mytext'];
 	echo $var1;
+	$var2=$_REQUEST['myradio'];
+	echo $var1;
+	$var3=$_REQUEST['mytext'];
+	echo $var1;
+	$var4=$_REQUEST['mytext'];
+	echo $var1;
+	$var5=$_REQUEST['mytext'];
+	echo $var1;
 	?>
 </body>
 </html>
